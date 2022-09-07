906da685454e93112f78c6c1f15460681a5b11ba
SauryaRay <93993772+SauryaRay@users.noreply.github.com>
2022-09-07 19:46:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+}
+ 
+function setup() 
+{
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    
+
+    
+}
+
+function draw() {
+    circle(30, 30, 20);
+    ellipse(76, 46, 55, 55);
+    rect(30, 110, 55, 55, 20);
+
+}
+
